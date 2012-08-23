1232509817cf65e8aaf25ec45cbd763b2ce38110
chxg23 <chxg23@gmail.com>
2012-08-23 03:28:16 +0000

    1.Set PM2 mode is OK. 2.Discovered TIME1 BUG, should set T1CC0H/T1CC0L register at first, than T1CNT would accumulation, and channel 1~4 works well.

--- app/timer_cc2530/util/util_timer.c
+++ app/timer_cc2530/util/util_timer.c
@@ -24,16 +24,23 @@ void timer1_init(void)
 {
 	/* Initalise Timer1(16bit).*/
 	HalTimerInit();
-	
-	/*
+
 	HalTimerConfig(HAL_TIMER_1,
 				   HAL_TIMER_MODE_CTC,
 				   HAL_TIMER_CHANNEL_0, 
 				   HAL_TIMER_CH_MODE_OUTPUT_COMPARE,
+				   FALSE,
+				   util_Timer1CallBack);
+	HalTimerStart(HAL_TIMER_1, 3906*3);		// 3s run timer1 ISR on channel 0.
+	
+	HalTimerConfig(HAL_TIMER_1,
+				   HAL_TIMER_MODE_CTC,
+				   HAL_TIMER_CHANNEL_1, 
+				   HAL_TIMER_CH_MODE_OUTPUT_COMPARE,
 				   TRUE,
 				   util_Timer1CallBack);
-	HalTimerStart(HAL_TIMER_1, 3906*3);		// 3s run timer1 ISR.
-	*/
+	HalTimerStart(HAL_TIMER_1, 3906*1);		// 1s run timer1 ISR on channel 1.
+	
 }
 
 /* ------------------------------------------------------------------------------------------------------
@@ -55,6 +62,7 @@ static void util_Timer1CallBack ( uint8 timerId, uint8 channel, uint8 channelMod
 		mac_host_beacon();
 		break;
 	case HAL_TIMER_CHANNEL_1:
+		halLedToggle(2);
 		break;
 	case HAL_TIMER_CHANNEL_2:
 		break;
