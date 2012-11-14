6cfd8a82f7312c799de3096e3d818f7f2127793a
chxg23@gmail.com <chxg23@gmail.com@cb055e1f-95e0-2694-f322-3c390eb83868>
2012-11-14 08:53:28 +0000

    1.Rebuild folder catalog; 2.Add energy.c&h files; 3.Compile project OK.

--- trunk/app/timer_cc2530/src/app/main.c
+++ trunk/app/timer_cc2530/src/app/main.c
@@ -1,15 +1,14 @@
-/*
-*********************************************************************************************************
-*
-*                                             hal_cc2530/Main
-*                                            INTRODUCTION DEMO
-*
-*
-* Filename      : Host.c
-* Version       : V1.01a
-* Programmer(s) : MC
-*********************************************************************************************************
-*/
+/* ------------------------------------------------------------------------------------------------------
+ *
+ *                                             hal_cc2530/Main
+ *                                            INTRODUCTION DEMO
+ *
+ *
+ * Filename      : Host.c
+ * Version       : V1.01a
+ * Programmer(s) : MC
+ * ------------------------------------------------------------------------------------------------------
+ */
 /* ------------------------------------------------------------------------------------------------------
  *                                            INCLUDE FILES
  * ------------------------------------------------------------------------------------------------------
@@ -37,25 +36,28 @@ extern void EnterSleepModeDisableInterruptsOnWakeup(void);
  */
 int main(void)
 {
-	/* Initialise clock, and put on INT.*/
-	halBoardInit();
+	halBoardInit();													/* Initialise clock, enable master interrupt.*/
 	
-	timer1_init();			// Initialise timer 1(16bit).
+//	timer1_init();													/* Initialise timer 1(16bit).*/
 	
-	mac_init();				// Initialise radio.
+//	mac_init();														/* Initialise MAC layer.*/
+	
+	energy_init();													/* Initialise energy I/O.*/
 	
 	SLEEPCMD = (SLEEPCMD & ~0x02) | 0x02;
-//	EnterSleepModeDisableInterruptsOnWakeup();
-	
+//	EnterSleepModeDisableInterruptsOnWakeup();						/* Enter sleep mode.*/
+
 	for(;;)
 	{
+		/*
 		if(sysflag & SYS_FLAG_SLEEP_START)
 		{
 			sysflag &= ~SYS_FLAG_SLEEP_START;
 			halLedClear(1);
 			EnterSleepModeDisableInterruptsOnWakeup();
 		}
-		mac_event_handle();
+		*/
+//		mac_event_handle();											/* Rx mode handle.*/
 	}
 }
 
