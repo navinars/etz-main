7744475dc3af9bc53155db72dca9e3717a520452
chxg23 <chxg23@gmail.com>
2013-03-16 02:09:57 +0000

    1.Add lwIP 1.4 code, but not okay.

--- app/Atmel/SAM3X_DP1000/src/ASF/common/boards/user_board/init.c
+++ app/Atmel/SAM3X_DP1000/src/ASF/common/boards/user_board/init.c
@@ -11,8 +11,20 @@
 
 void board_init(void)
 {
-	/* This function is meant to contain board-specific initialization code
-	 * for, e.g., the I/O pins. The initialization can rely on application-
-	 * specific board configuration, found in conf_board.h.
-	 */
+	/* Disable the watchdog */
+	WDT->WDT_MR = WDT_MR_WDDIS;
+		
+	/* Configure LED pins */
+	gpio_configure_pin(LED0_GPIO, LED0_FLAGS);
+	
+	gpio_configure_pin(PIO_PA12_IDX, PIO_TYPE_PIO_OUTPUT_1 | PIO_DEFAULT);
+
+	/* Configure UART pins */
+	gpio_configure_group(PINS_UART_PIO, PINS_UART, PINS_UART_FLAGS);
+	
+	/* Configure USART RXD pin */
+	gpio_configure_pin(PIN_USART1_RXD_IDX, PIN_USART1_RXD_FLAGS);
+	
+	/* Configure USART TXD pin */
+	gpio_configure_pin(PIN_USART1_TXD_IDX, PIN_USART1_TXD_FLAGS);
 }
--- app/Atmel/SAM3X_DP1000/src/config/conf_board.h
+++ app/Atmel/SAM3X_DP1000/src/config/conf_board.h
@@ -8,4 +8,57 @@
 #ifndef CONF_BOARD_H
 #define CONF_BOARD_H
 
+/*----------------------------------------------------------------------------*/
+/**
+ *  \page FreeRTOS task options
+ *  This page lists several definition related to the board operating frequency
+ *
+ *  \section Definitions
+ *  - \ref BOARD_FREQ_*
+ *  - \ref BOARD_MCK
+ */
+/*******************************************************************************
+                              任务堆栈大小定义
+*******************************************************************************/
+#define TASK_START_STACKSIZE			(configMINIMAL_STACK_SIZE + 50)
+#define TASK_LED_STACK_SIZE				(configMINIMAL_STACK_SIZE + 50)
+#define TASK_ETH_INT_STACK_SIZE			(configMINIMAL_STACK_SIZE + 50)
+#define TASK_LWIP_TCP_STACK_SIZE		(configMINIMAL_STACK_SIZE + 160)
+#define TASK_TCP_SERVER_STACK_SIZE		(configMINIMAL_STACK_SIZE + 150)
+
+
+/*******************************************************************************
+                              任务优先级
+*******************************************************************************/
+#define TASK_START_PRIORITY				(tskIDLE_PRIORITY + 9)
+#define TASK_ETH_INT_PRIORITY			(tskIDLE_PRIORITY + 8)
+#define TASK_TCPIP_PRIORITY				(tskIDLE_PRIORITY + 7)
+#define TASK_TCP_SERVER_PRIORITY		(tskIDLE_PRIORITY + 4)
+#define TASK_LED_PRIORITY				(tskIDLE_PRIORITY + 3)
+
+
+/**
+ * \file
+ * EMAC
+ *
+ * - BOARD_EMAC_PHY_ADDR: Phy MAC address
+ * - BOARD_EMAC_MODE_RMII: Enable RMII connection with the PHY
+ */
+#define PIN_EEMAC_EREFCK      PIO_PB0_IDX
+#define PIN_EMAC_ETXEN        PIO_PB1_IDX
+#define PIN_EMAC_ETX0         PIO_PB2_IDX
+#define PIN_EMAC_ETX1         PIO_PB3_IDX
+#define PIN_EMAC_ECRSDV       PIO_PB4_IDX
+#define PIN_EMAC_ERX0         PIO_PB5_IDX
+#define PIN_EMAC_ERX1         PIO_PB6_IDX
+#define PIN_EMAC_ERXER        PIO_PB7_IDX
+#define PIN_EMAC_EMDC         PIO_PB8_IDX
+#define PIN_EMAC_EMDIO        PIO_PB9_IDX
+#define PIN_EMAC_FLAGS        PIO_PERIPH_A | PIO_DEFAULT
+
+/** EMAC PHY address */
+#define BOARD_EMAC_PHY_ADDR  0
+/*! EMAC RMII mode */
+#define BOARD_EMAC_MODE_RMII 1
+
 #endif // CONF_BOARD_H
