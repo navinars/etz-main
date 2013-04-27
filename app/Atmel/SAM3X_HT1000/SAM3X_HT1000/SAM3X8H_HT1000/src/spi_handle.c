928daa592cdbdfb3332b7eca7f6f7e7f6bb82184
chxg23 <chxg23@gmail.com>
2013-04-27 07:00:56 +0000

    1.SPI is OK.

---
+++ app/Atmel/SAM3X_HT1000/SAM3X_HT1000/SAM3X8H_HT1000/src/spi_handle.c
@@ -0,0 +1,163 @@
+/* Standard includes. */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "conf_board.h"
+#include "conf_eth.h"
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+/* lwIP includes. */
+#include "lwip/api.h"
+#include "lwip/tcpip.h"
+#include "lwip/memp.h"
+#include "lwip/stats.h"
+#include "lwip/init.h"
+#include "lwip/sockets.h"
+#if ( (LWIP_VERSION) == ((1U << 24) | (3U << 16) | (2U << 8) | (LWIP_VERSION_RC)) )
+#include "netif/loopif.h"
+#endif
+
+/* Demo includes. */
+#include "portmacro.h"
+
+/* HT1000_BSP includes */
+#include "ht1000_spi.h"
+
+/* APP includes */
+#include "spi_handle.h"
+#include "net_handle.h"
+
+spi_data_send_t spi_t;
+
+static unsigned int crc16(unsigned char buff, unsigned int fcs);
+
+/**
+ * \brief 
+ * 
+ * \param uxPriority
+ * 
+ * \return void
+ */
+void vStartSpiTaskLauncher( unsigned portBASE_TYPE uxPriority )
+{
+	/* Spawn the Sentinel task. */
+	xTaskCreate( vSpiHandle, (const signed portCHAR *)"SPIHANDLE",
+				configMINIMAL_STACK_SIZE + 550, NULL, uxPriority,
+				(xTaskHandle *)NULL );
+}
+
+static unsigned int crc16(unsigned char buff, unsigned int fcs)
+{
+	unsigned char i,temp;
+	
+	fcs = buff^fcs;
+	
+	for (i = 0;i < 8;i ++)
+	{
+		temp = fcs&0x01;
+		
+		if (temp == 0)
+		fcs = fcs >> 1;
+		else
+		{
+			fcs = fcs >> 1;
+			fcs = fcs^0xa001;
+		}
+	}
+	return fcs;
+}
+
+/**
+ * \brief Sum CRC number
+ * 
+ * \param ptr
+ * \param length
+ * 
+ * \return unsigned int
+ */
+unsigned int Crc16CheckSum(unsigned char *ptr, unsigned char length)
+{
+	unsigned int xym;
+	
+	xym = 0xffff;
+	
+	while(length --)
+	{
+		xym = crc16(*ptr, xym);
+		ptr ++;
+	}
+	return xym;
+}
+
+/**
+ * \brief SPI data handle function.
+ * 
+ * \param pdata
+ * 
+ * \return 
+ */
+static void spi_data_handle ( spi_data_send_t* pdata )
+{
+	int ret;
+	
+//	RS232printf("\r\nSPI data receive...");
+//	ret = lwip_write(pdata->port, &pdata->buf[0], pdata->len);
+	
+	if(ret < 0){
+		RS232printf("\n\rAn error occurred lwip_write..");
+	}
+}
+
+/**
+ * \brief SPI handle task..
+ * 
+ * \param 
+ * \param 
+ * 
+ * \return None
+ */
+portTASK_FUNCTION_PROTO( vSpiHandle, pvParameters )
+{
+	u_char	len;
+	
+	(void)pvParameters;
+	memset(&spi_t, 0, sizeof(spi_data_send_t));						/* Clear struct spi_t. */
+	
+	while(1)
+	{
+		if (xSemaNetHandle != NULL)
+		{
+			if(xSemaphoreGive( xSemaNetHandle ) == pdTRUE)
+			{
+				u_short crc;
+				
+				RS232printf("\r\nSPI_HANDLE is running...");
+				
+				len = spi_t.len - 0x30;
+				
+				crc = Crc16CheckSum(&spi_t.buf[1], len);
+				*((u_short *)&spi_t.buf[len]) = crc;
+				
+				spi_csn0_disable();
+				spi_master_transfer(spi_t.buf, spi_t.len + 2);
+				vTaskDelay(20);										/* Wait 20 millisecond.*/
+				spi_master_transfer(spi_t.buf, spi_t.len + 2);		/* Update to spi.buf[].*/
+				spi_csn0_enable();
+				
+				RS232printf("0x%x", spi_t.buf[0]);
+				
+				// enable spi receive data function.
+				spi_data_handle( &spi_t );
+				
+				memset(&spi_t, 0, sizeof(spi_data_send_t));			/* clear struct...*/
+			}
+		}
+		vTaskDelay(1);
+	}
+}
+
---
+++ app/Atmel/SAM3X_HT1000/SAM3X_HT1000/SAM3X8H_HT1000/src/spi_handle.h
@@ -0,0 +1,40 @@
+#ifndef BASIC_SPI_HANDLE_H
+#define BASIC_SPI_HANDLE_H
+
+#include "portmacro.h"
+#include <semphr.h>
+
+xSemaphoreHandle xSemaNetHandle;
+
+typedef struct{
+	bool alloc;
+	int port;
+	u_char len;
+	u_char buf[8];
+}spi_data_send_t ;
+
+extern spi_data_send_t spi_t;
+
+/*! \brief WEB server main task
+ *
+ *  \param pvParameters   Input. Not Used.
+ *
+ */
+extern unsigned int Crc16CheckSum(unsigned char *ptr, unsigned char length);
+
+/*! \brief WEB server main task
+ *
+ *  \param pvParameters   Input. Not Used.
+ *
+ */
+void vStartSpiTaskLauncher( unsigned portBASE_TYPE uxPriority );
+
+/*! \brief WEB server main task
+ *
+ *  \param pvParameters   Input. Not Used.
+ *
+ */
+portTASK_FUNCTION_PROTO( vSpiHandle, pvParameters );
+
+#endif
+
