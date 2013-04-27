3b5970cc1ab7aa4f05bd4b67499a2a93a578a7f2
chxg23@gmail.com <chxg23@gmail.com@cb055e1f-95e0-2694-f322-3c390eb83868>
2013-04-27 07:00:56 +0000

    1.SPI is OK.

---
+++ trunk/app/Atmel/SAM3X_HT1000/SAM3X_HT1000/SAM3X8H_HT1000/src/config/conf_board.h
@@ -0,0 +1,116 @@
+/**
+ * \file
+ *
+ * \brief User board configuration template
+ *
+ */
+
+#ifndef CONF_BOARD_H
+#define CONF_BOARD_H
+
+/*----------------------------------------------------------------------------*/
+/**
+ *  \page FreeRTOS task options
+ *  This page lists several definition related to the board operating frequency
+ *
+ *  \section Definitions
+ *  - \ref BOARD_FREQ_*
+ *  - \ref BOARD_MCK
+ */
+/*******************************************************************************
+                              任务堆栈大小定义
+*******************************************************************************/
+#define TASK_START_STACKSIZE			(configMINIMAL_STACK_SIZE + 50)
+#define TASK_LED_STACK_SIZE				(configMINIMAL_STACK_SIZE + 50)
+#define TASK_ETH_INT_STACK_SIZE			(configMINIMAL_STACK_SIZE + 50)
+#define TASK_TCP_SERVER_STACK_SIZE		(configMINIMAL_STACK_SIZE + 550)
+
+
+/*******************************************************************************
+                              任务优先级
+*******************************************************************************/
+#define TASK_START_PRIORITY				(tskIDLE_PRIORITY + 19)
+#define TASK_LED_PRIORITY				(tskIDLE_PRIORITY + 18)
+#define TASK_START_ETH_PRIORITY			(tskIDLE_PRIORITY + 17)
+#define TASK_TCP_SERVER_PRIORITY		(tskIDLE_PRIORITY + 16)
+#define TASK_SPI_HANDLE_PRIORITY		(tskIDLE_PRIORITY + 15)
+
+
+/**
+ * \file
+ * EMAC
+ *
+ * - BOARD_EMAC_PHY_ADDR: Phy MAC address
+ * - BOARD_EMAC_MODE_RMII: Enable RMII connection with the PHY
+ */
+#define PIN_EEMAC_EREFCK      PIO_PB0_IDX
+#define PIN_EMAC_ETXEN        PIO_PB1_IDX
+#define PIN_EMAC_ETX0         PIO_PB2_IDX
+#define PIN_EMAC_ETX1         PIO_PB3_IDX
+#define PIN_EMAC_ECRSDV       PIO_PB4_IDX
+#define PIN_EMAC_ERX0         PIO_PB5_IDX
+#define PIN_EMAC_ERX1         PIO_PB6_IDX
+#define PIN_EMAC_ERXER        PIO_PB7_IDX
+#define PIN_EMAC_EMDC         PIO_PB8_IDX
+#define PIN_EMAC_EMDIO        PIO_PB9_IDX
+#define PIN_EMAC_FLAGS        PIO_PERIPH_A | PIO_DEFAULT
+
+/* ------------------------------------------------------------------------ */
+/* SPI                                                                      */
+/* ------------------------------------------------------------------------ */
+/*! SPI0 MISO pin definition. */
+#define SPI0_MISO_GPIO        (PIO_PA25_IDX)
+#define SPI0_MISO_FLAGS       (PIO_PERIPH_A | PIO_DEFAULT)
+/*! SPI0 MOSI pin definition. */
+#define SPI0_MOSI_GPIO        (PIO_PA26_IDX)
+#define SPI0_MOSI_FLAGS       (PIO_PERIPH_A | PIO_DEFAULT)
+/*! SPI0 SPCK pin definition. */
+#define SPI0_SPCK_GPIO        (PIO_PA27_IDX)
+#define SPI0_SPCK_FLAGS       (PIO_PERIPH_A | PIO_DEFAULT)
+
+/*! SPI0 chip select 0 pin definition. (Only one configuration is possible) */
+#define SPI0_NPCS0_GPIO            (PIO_PA28_IDX)
+#define SPI0_NPCS0_FLAGS           (PIO_PERIPH_A | PIO_DEFAULT)
+/*! SPI0 chip select 1 pin definition. (multiple configurations are possible) */
+#define SPI0_NPCS1_PA29_GPIO       (PIO_PA29_IDX)
+#define SPI0_NPCS1_PA29_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define SPI0_NPCS1_PB20_GPIO       (PIO_PB20_IDX)
+#define SPI0_NPCS1_PB20_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+/*! SPI0 chip select 2 pin definition. (multiple configurations are possible) */
+#define SPI0_NPCS2_PA30_GPIO       (PIO_PA30_IDX)
+#define SPI0_NPCS2_PA30_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define SPI0_NPCS2_PB21_GPIO       (PIO_PB21_IDX)
+#define SPI0_NPCS2_PB21_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+/*! SPI0 chip select 3 pin definition. (multiple configurations are possible) */
+#define SPI0_NPCS3_PA31_GPIO       (PIO_PA31_IDX)
+#define SPI0_NPCS3_PA31_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define SPI0_NPCS3_PB23_GPIO       (PIO_PB23_IDX)
+#define SPI0_NPCS3_PB23_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+
+/** Enable Com Port. */
+#define CONF_BOARD_UART_CONSOLE
+
+/** Usart Hw ID used by the console (UART). */
+#define CONSOLE_UART_ID          ID_UART
+
+/** SPI0 MACRO definition */
+#define CONF_BOARD_SPI0
+
+/** SPI0 slave select0 MACRO definition */
+#define CONF_BOARD_SPI0_NPCS0
+
+/** Spi Hw ID . */
+#define SPI_ID          ID_SPI0
+
+/** SPI base address for SPI master mode*/
+#define SPI_MASTER_BASE      SPI0
+/** SPI base address for SPI slave mode, (on different board) */
+#define SPI_SLAVE_BASE       SPI0
+/** EMAC PHY address */
+#define BOARD_EMAC_PHY_ADDR  0
+/*! EMAC RMII mode */
+#define BOARD_EMAC_MODE_RMII 1
+
+#define RS232printf					//printf
+
+#endif // CONF_BOARD_H
