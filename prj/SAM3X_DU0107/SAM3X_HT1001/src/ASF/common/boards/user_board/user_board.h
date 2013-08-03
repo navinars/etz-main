845c2b918e36b97c68838f8f371d56da14161b86
chxg23 <chxg23@gmail.com>
2013-08-03 03:07:36 +0000

    1.lwIp is OK; 2.cc1101 send and recieve is OK; 3.Add freeRTOS 7.3.0 + Trace.

---
+++ prj/SAM3X_DU0107/SAM3X_HT1001/src/ASF/common/boards/user_board/user_board.h
@@ -0,0 +1,168 @@
+/**
+ * \file
+ *
+ * \brief User board definition template
+ *
+ */
+
+ /* This file is intended to contain definitions and configuration details for
+ * features and devices that are available on the board, e.g., frequency and
+ * startup time for an external crystal, external memory devices, LED and USART
+ * pins.
+ */
+
+#ifndef USER_BOARD_H
+#define USER_BOARD_H
+
+#include <conf_board.h>
+
+/*----------------------------------------------------------------------------*/
+/**
+ *  \page sam3u_ek_opfreq SAM3U-EK - Operating frequencies
+ *  This page lists several definition related to the board operating frequency
+ *
+ *  \section Definitions
+ *  - \ref BOARD_FREQ_*
+ *  - \ref BOARD_MCK
+ */
+
+/** Board oscillator settings */
+#define BOARD_FREQ_SLCK_XTAL		(32768U)
+#define BOARD_FREQ_SLCK_BYPASS		(32768U)
+#define BOARD_FREQ_MAINCK_XTAL		(12000000U)
+#define BOARD_FREQ_MAINCK_BYPASS	(12000000U)
+
+/** Master clock frequency */
+#define BOARD_MCK					CHIP_FREQ_CPU_MAX
+
+/** board main clock xtal statup time */
+#define BOARD_OSC_STARTUP_US   15625
+
+#define CONSOLE_UART			   UART
+#define CONSOLE_UART_ID            ID_UART
+
+//! LED #0 pin definition (BLUE).
+#define LED_0_NAME    "green LED D2"
+#define LED0_GPIO     (PIO_PA2_IDX)
+#define LED0_FLAGS    (PIO_TYPE_PIO_OUTPUT_1 | PIO_DEFAULT)
+#define LED0_ACTIVE_LEVEL 0
+//! LED #1 pin definition (BLUE).
+#define LED_1_NAME    "green LED D1"
+#define LED1_GPIO     (PIO_PA3_IDX)
+#define LED1_FLAGS    (PIO_TYPE_PIO_OUTPUT_1 | PIO_DEFAULT)
+#define LED1_ACTIVE_LEVEL 0
+
+
+#define RESTKEY_GPIO     (PIO_PA20_IDX)
+
+//! UART pins (UTXD0 and URXD0) definitions, PA8,9.
+#define PINS_UART		(PIO_PA8A_URXD | PIO_PA9A_UTXD)
+#define PINS_UART_FLAGS	(PIO_PERIPH_A | PIO_DEFAULT)
+
+#define PINS_UART_MASK	(PIO_PA8A_URXD | PIO_PA9A_UTXD)
+#define PINS_UART_PIO	PIOA
+#define PINS_UART_ID	ID_PIOA
+#define PINS_UART_TYPE	PIO_PERIPH_A
+#define PINS_UART_ATTR	PIO_DEFAULT
+
+//! USART0
+#define PIN_USART0_RXD    {0x1 << 19, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_USART0_RXD_IDX        (PIO_PA19_IDX)
+#define PIN_USART0_RXD_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART0_TXD    {0x1 << 18, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_USART0_TXD_IDX        (PIO_PA18_IDX)
+#define PIN_USART0_TXD_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART0_CTS    {0x1 << 8, PIOB, ID_PIOB, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_USART0_CTS_IDX        (PIO_PA8_IDX)
+#define PIN_USART0_CTS_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART0_RTS    {0x1 << 7, PIOB, ID_PIOB, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_USART0_RTS_IDX        (PIO_PA7_IDX)
+#define PIN_USART0_RTS_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART0_SCK    {0x1 << 17, PIOA,ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_USART0_SCK_IDX        (PIO_PA17_IDX)
+#define PIN_USART0_SCK_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+
+//! USART1
+#define PIN_USART1_RXD    {0x1 << 21, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_USART1_RXD_IDX        (PIO_PA21_IDX)
+#define PIN_USART1_RXD_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART1_TXD    {0x1 << 20, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}
+#define PIN_USART1_TXD_IDX        (PIO_PA20_IDX)
+#define PIN_USART1_TXD_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART1_CTS    {0x1 << 23, PIOA, ID_PIOA, PIO_PERIPH_B, PIO_DEFAULT}
+#define PIN_USART1_CTS_IDX        (PIO_PA23_IDX)
+#define PIN_USART1_CTS_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+#define PIN_USART1_RTS    {0x1 << 22, PIOA, ID_PIOA, PIO_PERIPH_B, PIO_DEFAULT}
+#define PIN_USART1_RTS_IDX        (PIO_PA22_IDX)
+#define PIN_USART1_RTS_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+#define PIN_USART1_EN     {0x1 << 24, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
+#define PIN_USART1_EN_IDX         (PIO_PA24_IDX)
+#define PIN_USART1_EN_FLAGS       (PIO_OUTPUT_0 | PIO_DEFAULT)
+#define PIN_USART1_SCK    {0x1 << 24, PIOA, ID_PIOA, PIO_PERIPH_B, PIO_DEFAULT}
+#define PIN_USART1_SCK_IDX        (PIO_PA24_IDX)
+#define PIN_USART1_SCK_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+
+/**
+ * \file
+ * EMAC
+ *
+ * - BOARD_EMAC_PHY_ADDR: Phy MAC address
+ * - BOARD_EMAC_MODE_RMII: Enable RMII connection with the PHY
+ */
+#define PIN_EEMAC_EREFCK      PIO_PB0_IDX
+#define PIN_EMAC_ETXEN        PIO_PB1_IDX
+#define PIN_EMAC_ETX0         PIO_PB2_IDX
+#define PIN_EMAC_ETX1         PIO_PB3_IDX
+#define PIN_EMAC_ECRSDV       PIO_PB4_IDX
+#define PIN_EMAC_ERX0         PIO_PB5_IDX
+#define PIN_EMAC_ERX1         PIO_PB6_IDX
+#define PIN_EMAC_ERXER        PIO_PB7_IDX
+#define PIN_EMAC_EMDC         PIO_PB8_IDX
+#define PIN_EMAC_EMDIO        PIO_PB9_IDX
+#define PIN_EMAC_FLAGS        PIO_PERIPH_A | PIO_DEFAULT
+
+/* ------------------------------------------------------------------------ */
+/* SPI                                                                      */
+/* ------------------------------------------------------------------------ */
+/*! SPI0 MISO pin definition. */
+#define SPI0_MISO_GPIO        (PIO_PA25_IDX)
+#define SPI0_MISO_FLAGS       (PIO_PERIPH_A | PIO_DEFAULT)
+#define SPI0_MISO_PORT		  (IOPORT_PIOA)
+#define SPI0_MISO_MASK		  (PIO_PA25)
+
+/*! SPI0 MOSI pin definition. */
+#define SPI0_MOSI_GPIO        (PIO_PA26_IDX)
+#define SPI0_MOSI_FLAGS       (PIO_PERIPH_A | PIO_DEFAULT)
+
+/*! SPI0 SPCK pin definition. */
+#define SPI0_SPCK_GPIO        (PIO_PA27_IDX)
+#define SPI0_SPCK_FLAGS       (PIO_PERIPH_A | PIO_DEFAULT)
+
+/*! SPI0 chip select 0 pin definition. (Only one configuration is possible) */
+#define SPI0_NPCS0_GPIO            (PIO_PA28_IDX)
+#define SPI0_NPCS0_FLAGS           (PIO_PERIPH_A | PIO_DEFAULT)
+/*! SPI0 chip select 1 pin definition. (multiple configurations are possible) */
+#define SPI0_NPCS1_PA29_GPIO       (PIO_PA29_IDX)
+#define SPI0_NPCS1_PA29_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define SPI0_NPCS1_PB20_GPIO       (PIO_PB20_IDX)
+#define SPI0_NPCS1_PB20_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+/*! SPI0 chip select 2 pin definition. (multiple configurations are possible) */
+#define SPI0_NPCS2_PA30_GPIO       (PIO_PA30_IDX)
+#define SPI0_NPCS2_PA30_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define SPI0_NPCS2_PB21_GPIO       (PIO_PB21_IDX)
+#define SPI0_NPCS2_PB21_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+/*! SPI0 chip select 3 pin definition. (multiple configurations are possible) */
+#define SPI0_NPCS3_PA31_GPIO       (PIO_PA31_IDX)
+#define SPI0_NPCS3_PA31_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define SPI0_NPCS3_PB23_GPIO       (PIO_PB23_IDX)
+#define SPI0_NPCS3_PB23_FLAGS      (PIO_PERIPH_B | PIO_DEFAULT)
+
+/** CC1101 GPIO pin definition. */
+#define CC1101_GPIO0_PIO          PIOA
+#define CC1101_INT_ID           ID_PIOA
+#define CC1101_GPIO0			(1 << 23)
+#define CC1101_INT_ATTR         (PIO_PULLUP | PIO_DEBOUNCE | PIO_IT_FALL_EDGE)
+#define CC1101_GPIO0_GPIO		(PIO_PA23_IDX)
+#define CC1101_GPIO0_FLAGS      (PIO_INPUT | PIO_PULLUP)
+
+#endif // USER_BOARD_H
---
+++ prj/SAM3X_DU0107/SAM3X_HT1001/src/BSP/cc1101_radio.c
@@ -0,0 +1,384 @@
+
+/* ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+ *   MRFI (Minimal RF Interface)
+ *   Radios: CC2500, CC1100, CC1101
+ *   Primary code file for supported radios.
+ * ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+ */
+
+/* ------------------------------------------------------------------------------------------------
+ *                                          Includes
+ * ------------------------------------------------------------------------------------------------
+ */
+#include "asf.h"
+#include "cc1101_defs.h"
+#include "cc1101_port.h"
+#include "cc1101_radio.h"
+
+
+/* ------------------------------------------------------------------------------------------------
+ *                                          Defines
+ * ------------------------------------------------------------------------------------------------
+ */
+#define DUMMY_BYTE                  0xDB
+
+#define READ_BIT                    0x80
+#define BURST_BIT                   0x40
+
+/* ------------------------------------------------------------------------------------------------
+ *                                           Macros
+ * ------------------------------------------------------------------------------------------------
+ */
+
+
+/* ------------------------------------------------------------------------------------------------
+ *                                    Local Constants
+ * ------------------------------------------------------------------------------------------------
+ */
+
+/* ------------------------------------------------------------------------------------------------
+ *                                       Local Prototypes
+ * ------------------------------------------------------------------------------------------------
+ */
+
+
+/* ------------------------------------------------------------------------------------------------
+ *                                       Local Variables
+ * ------------------------------------------------------------------------------------------------
+ */
+static uint8_t mrfiRadioState = MRFI_RADIO_STATE_UNKNOWN;
+
+
+// Product = CC1100
+// Crystal accuracy = 40 ppm
+// X-tal frequency = 26 MHz
+// RF output power = 0 dBm
+// RX filterbandwidth = 540.000000 kHz
+// Deviation = 0.000000
+// Return state:  Return to RX state upon leaving either TX or RX
+// Datarate = 250.000000 kbps
+// Modulation = (7) MSK
+// Manchester enable = (0) Manchester disabled
+// RF Frequency = 868.000000 MHz
+// Channel spacing = 199.951172 kHz
+// Channel number = 0
+// Optimization = Sensitivity
+// Sync mode = (3) 30/32 sync word bits detected
+// Format of RX/TX data = (0) Normal mode, use FIFOs for RX and TX
+// CRC operation = (1) CRC calculation in TX and CRC check in RX enabled
+// Forward Error Correction = (0) FEC disabled
+// Length configuration = (1) Variable length packets, packet length configured by the first received byte after sync word.
+// Packetlength = 255
+// Preamble count = (2)  4 bytes
+// Append status = 1
+// Address check = (0) No address check
+// FIFO autoflush = 0
+// Device address = 0
+// GDO0 signal selection = ( 6) Asserts when sync word has been sent / received, and de-asserts at the end of the packet
+// GDO2 signal selection = (11) Serial Clock
+
+/**
+ * \brief 
+ * 
+ * \param 
+ * 
+ * \return void
+ */
+static void writeRFSettings(void)
+{
+	Mrfi_SpiWriteReg(IOCFG0,0x06);
+	Mrfi_SpiWriteReg(PKTCTRL0,0x05);
+	Mrfi_SpiWriteReg(CHANNR,0xAA);
+	Mrfi_SpiWriteReg(FSCTRL1,0x08);
+	Mrfi_SpiWriteReg(FREQ2,0x21);
+	Mrfi_SpiWriteReg(FREQ1,0x62);
+	Mrfi_SpiWriteReg(FREQ0,0x76);
+	Mrfi_SpiWriteReg(MDMCFG4,0xC7);
+	Mrfi_SpiWriteReg(MDMCFG3,0x83);
+	Mrfi_SpiWriteReg(MDMCFG2,0x93);
+	Mrfi_SpiWriteReg(DEVIATN,0x40);
+	Mrfi_SpiWriteReg(MCSM0,0x18);
+	Mrfi_SpiWriteReg(FOCCFG,0x16);
+	Mrfi_SpiWriteReg(AGCCTRL2,0x43);
+	Mrfi_SpiWriteReg(WORCTRL,0xFB);
+	Mrfi_SpiWriteReg(FSCAL3,0xE9);
+	Mrfi_SpiWriteReg(FSCAL2,0x2A);
+	Mrfi_SpiWriteReg(FSCAL1,0x00);
+	Mrfi_SpiWriteReg(FSCAL0,0x1F);
+	Mrfi_SpiWriteReg(TEST2,0x81);
+	Mrfi_SpiWriteReg(TEST1,0x35);
+	Mrfi_SpiWriteReg(TEST0,0x09);
+
+	//SPIWriteReg(CCxxx0_IOCFG2,   0x0e);
+	Mrfi_SpiWriteReg(IOCFG2,   0x0b);
+	Mrfi_SpiWriteReg(FSCTRL0,  0x00);
+	Mrfi_SpiWriteReg(BSCFG,    0x6c);
+	Mrfi_SpiWriteReg(FSTEST,   0x59);
+	Mrfi_SpiWriteReg(PKTCTRL1, 0x04);
+	//SPIWriteReg(CCxxx0_ADDR,     0x00);
+	Mrfi_SpiWriteReg(PKTLEN,   0xff);
+	Mrfi_SpiWriteReg(AGCCTRL1,0x41);
+	Mrfi_SpiWriteReg(AGCCTRL0,0xb2);
+
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * @fn          spiRegAccess
+ *
+ * @brief       Read value from radio register.
+ *
+ * @param       none
+ *
+ * @return      none
+ *
+ */
+static uint8_t spiRegAccess(uint8_t addrByte, uint8_t writeValue)
+{
+	uint8_t readValue;
+	
+	/* disable interrupts that use SPI */
+//	MRFI_SPI_ENTER_CRITICAL_SECTION(s);
+	
+	SPI_DRIVE_CSN_HIGH();
+	SPI_DRIVE_CSN_LOW();
+	
+	while(SPI_SO_IS_HIGH());
+	
+	/* send register address byte, the read/write bit is already configured */
+	SpiWriteByte(addrByte);
+	
+	/*
+	*  Send the byte value to write.  If this operation is a read, this value
+	*  is not used and is just dummy data.  Wait for SPI access to complete.
+	*/
+	readValue = SpiWriteByte(writeValue);
+	
+	/* turn off chip select; enable interrupts that call SPI functions */
+	SPI_DRIVE_CSN_HIGH();
+//	MRFI_SPI_EXIT_CRITICAL_SECTION(s);
+	
+	/* return the register value */
+	return(readValue);
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * @fn          mrfiSpiReadReg
+ *
+ * @brief       Read value from radio register.
+ *
+ * @param       none
+ *
+ * @return      none
+ *
+ */
+uint8_t Mrfi_SpiReadReg(uint8_t addr)
+{
+	/*
+	*  The burst bit is set to allow access to read-only status registers.
+	*  This does not affect normal register reads.
+	*/
+	return( spiRegAccess(addr | BURST_BIT | READ_BIT, DUMMY_BYTE) );
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * @fn          mrfiSpiWriteReg
+ *
+ * @brief       Initialize radio.
+ *
+ * @param       none
+ *
+ * @return      none
+ *
+ */
+void Mrfi_SpiWriteReg(uint8_t addr, uint8_t value)
+{
+	spiRegAccess(addr, value);
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * \brief 
+ * 
+ * \param pPacket
+ * \param txType
+ * 
+ * \return uint8_t
+ */
+uint8_t Mrfi_SpiCmdStrobe(uint8_t cmd)
+{
+	uint8_t statusByte;
+	
+	/* disable interrupts that use SPI */
+//	MRFI_SPI_ENTER_CRITICAL_SECTION(s);
+	
+	/* turn chip select "off" and then "on" to clear any current SPI access */
+	SPI_DRIVE_CSN_HIGH();
+	SPI_DRIVE_CSN_LOW();
+	
+	while(SPI_SO_IS_HIGH());
+	
+	/* send the command strobe, wait for SPI access to complete */
+	statusByte = SpiWriteByte(cmd);	
+	
+	/* turn off chip select; enable interrupts that call SPI functions */
+	SPI_DRIVE_CSN_HIGH();
+	
+//	MRFI_SPI_EXIT_CRITICAL_SECTION(s);
+	
+	/* return the status byte */
+	return(statusByte);
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * \brief 
+ * 
+ * \param pPacket
+ * \param txType
+ * 
+ * \return uint8_t
+ */
+static uint8_t spiBurstFifoAccess(uint8_t addrByte, uint8_t * pData, uint8_t len)
+{
+	/* disable interrupts that use SPI */
+//	MRFI_SPI_ENTER_CRITICAL_SECTION(s);
+
+	/* turn chip select "off" and then "on" to clear any current SPI access */
+	SPI_DRIVE_CSN_HIGH();
+	SPI_DRIVE_CSN_LOW();
+	
+	while(SPI_SO_IS_HIGH());
+
+	/* send FIFO access command byte, wait for SPI access to complete */
+	SpiWriteByte(addrByte);
+	SPIWriteArrayBytes(pData, len);
+	
+	/* turn off chip select; enable interrupts that call SPI functions */
+	SPI_DRIVE_CSN_HIGH();
+//	MRFI_SPI_EXIT_CRITICAL_SECTION(s);
+	
+	return 1;
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * \brief 
+ * 
+ * \param pPacket
+ * \param txType
+ * 
+ * \return uint8_t
+ */
+uint8_t Mrfi_SpiWriteTxFifo(uint8_t * pData, uint8_t len)
+{
+  return spiBurstFifoAccess(TXFIFO | BURST_BIT, pData, len);
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * \brief 
+ * 
+ * \param pPacket
+ * \param txType
+ * 
+ * \return uint8_t
+ */
+uint8_t Mrfi_SpiReadRxFifo(uint8_t * pData, uint8_t len)
+{
+  return spiBurstFifoAccess(RXFIFO | BURST_BIT | READ_BIT, pData, len);
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * \brief 
+ * 
+ * \param pPacket
+ * \param txType
+ * 
+ * \return uint8_t
+ */
+uint8_t Radio_Transmit(uint8_t * pPacket, uint8_t len)
+{
+	Mifi_DisableInt();												// Disable cc1101 interrupt.
+	
+	Mrfi_SpiWriteReg(TXFIFO, len);
+	Mrfi_SpiWriteTxFifo(pPacket, len);
+	
+	{
+		uint8_t tx_status;
+		
+		tx_status = Mrfi_SpiReadReg(TXBYTES);
+		if(tx_status != 0x05)
+			tx_status = 0;
+	}
+	
+	Mrfi_SpiCmdStrobe(SIDLE);
+	Mrfi_SpiCmdStrobe(STX);
+	while(!Spi_CheckGpio0());
+	while(Spi_CheckGpio0());
+	Mrfi_SpiCmdStrobe(SFTX);										// Clear TxFIFOs..
+	Mrfi_SpiCmdStrobe(SIDLE);
+	Mrfi_SpiCmdStrobe(SRX);											// Go to RX mode.
+	
+	Mifi_EnableInt();												// Enable cc1101 interrupt.
+	
+	return 1;
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * @fn          radio_init
+ *
+ * @brief       Initialize radio.
+ *
+ * @param       none
+ *
+ * @return      none
+ *
+ */
+void Radio_Init(void)
+{
+	
+	/* Initial radio state is IDLE state */
+	mrfiRadioState = MRFI_RADIO_STATE_IDLE;
+
+	spi_set_clock_configuration(1);									// SPI mode init on 1000000(1MHz)..
+	
+	/* pulse CSn low then high */
+	SPI_DRIVE_CSN_LOW();
+	
+	Mrfi_DelayUsec(100);
+	
+	SPI_DRIVE_CSN_HIGH();
+
+	/* hold CSn high for at least 40 microseconds */
+	Mrfi_DelayUsec(400);
+	
+	/* pull CSn low and wait for SO to go low */
+	SPI_DRIVE_CSN_LOW();
+	while (SPI_SO_IS_HIGH());
+	
+	/* send the command strobe, wait for SPI access to complete */
+	SpiWriteByte(SRES);
+	
+	/* wait for SO to go low again, reset is complete at that point */
+	while (SPI_SO_IS_HIGH());
+
+	/* return CSn pin to its default high level */
+	SPI_DRIVE_CSN_HIGH();
+	
+	/* initialize radio registers */
+	writeRFSettings();
+	
+	/* Initial radio state is IDLE state */
+	mrfiRadioState = MRFI_RADIO_STATE_IDLE;
+
+	Mrfi_SpiWriteReg(IOCFG0, 0x06);									// Set SYNC signal interrupt.
+	
+	{
+		uint8_t d;
+		d = Mrfi_SpiReadReg(IOCFG0);
+		if (d == 0)
+			while(1);
+	}
+
+	Mrfi_SpiCmdStrobe(SIDLE);
+	Mrfi_SpiCmdStrobe(SRX);											// Access Receive mode..
+	
+	Mifi_ConfigInt();												// Config GPIO0 interrupt.
+}
+
